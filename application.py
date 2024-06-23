2585446ec4f8f6e31fe2c06b6af4ff8523c7035d
kotipalligunasatya <124607727+kotipalligunasatya@users.noreply.github.com>
2024-06-23 21:26:59 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ app=application
 # def home_page():
 #     return render_template('form.html')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/predict',methods=['GET','POST'])
 
 def predict_datapoint():
     if request.method=='GET':
