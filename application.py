7b29bd7b0a1d9a297aa193883af0313c3e34b1dd
kotipalligunasatya <124607727+kotipalligunasatya@users.noreply.github.com>
2024-06-23 18:18:58 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,11 +6,11 @@ application=Flask(__name__)
 
 app=application
 
-@app.route('/')
-def home_page():
-    return render_template('form.html')
+# @app.route('/')
+# def home_page():
+#     return render_template('form.html')
 
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 
 def predict_datapoint():
     if request.method=='GET':
